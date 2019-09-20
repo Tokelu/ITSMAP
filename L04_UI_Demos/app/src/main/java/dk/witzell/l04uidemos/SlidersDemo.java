58f325c09a117fbb0c18c586b5083296d171928f
Tonni <tonni@witzell.dk>
2019-09-20 09:16:34 +0200

    All assignments added

---
+++ L04_UI_Demos/app/src/main/java/dk/witzell/l04uidemos/SlidersDemo.java
@@ -0,0 +1,14 @@
+package dk.witzell.l04uidemos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SlidersDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sliders_demo);
+    }
+}
