79545084174ce01adc85e80bc9400451f7da7ec9
Tonni <tonni@witzell.dk>
2019-09-28 19:46:34 +0200

    -

--- L04_UI_Demos/app/src/main/java/dk/witzell/l04uidemos/PickerDemoActivity.java
+++ L04_UI_Demos/app/src/main/java/dk/witzell/l04uidemos/PickerDemoActivity.java
@@ -4,11 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
 public class PickerDemoActivity extends AppCompatActivity {
+
+    //Tag constant for logging
+    public static final String TAG = "Picker Demo Events";
+
+    //Declare widgets
+    private NumberPicker npPickerDemo;
+    private TextView    txtViewPickerDemo;
+    private Button      btnPickerDemoOK;
+    private Button      btnPickerDemoCancel;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picker_demo);
+
+
+        //Get UI widgets
+        btnPickerDemoOK     = findViewById(R.id.btnPickerDemoOK);
+        btnPickerDemoCancel = findViewById(R.id.btnPickerDemoCancel);
+        npPickerDemo        = findViewById(R.id.npPickerDemo);
+
+
+        //Setting min/max values for picker
+        npPickerDemo.setMinValue(1);
+        npPickerDemo.setMaxValue(1000);
+
+        //Creating listener for onValueChanged
+        npPickerDemo.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                int valuePicker1 = npPickerDemo.getValue();
+                Log.d(TAG, "picker value" + valuePicker1 + "");
+            }
+        });
+
+
     }
 }
